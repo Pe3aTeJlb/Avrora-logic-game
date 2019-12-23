ddd92e75b13f982e7eb7cb1a1669314dd4e67855
Pe3aTeJlb <shepuhin@yandex.ru>
2019-12-23 20:46:02 +0300

    Initial Commit

--- src/com/pplosstudio/avroralogicgame/client/CirSim.java
+++ src/com/pplosstudio/avroralogicgame/client/CirSim.java
@@ -67,19 +67,35 @@ import com.google.gwt.user.client.Window.Navigator;
 public class CirSim {
 
 	static CirSim theSim;
-	Canvas cv;
+	
 	final int FASTTIMER=16;
+	
+
+    //static final int POSTGRABSQ=25;
+    //static final int MINPOSTGRABSIZE = 256;
+	
+    Canvas cv;
+    Context2d cvcontext;
+    Canvas backcv;
+    Context2d backcontext;
+    
+	Rectangle circuitArea;
+	
+	double scopeHeightFraction = 0.2;
 	static final int MENUBARHEIGHT=30;
     static int VERTICALPANELWIDTH=166; // default
-    static final int POSTGRABSQ=25;
-    static final int MINPOSTGRABSIZE = 256;
     
+     DockLayoutPanel layoutPanel;
      MenuBar mainBar;
      MenuBar extrasBar;
      CheckboxMenuItem alternativeColorCheckItem;
      CheckboxMenuItem printableCheckItem;
+
      
-     MenuItem item;
+     int gridSize, gridMask, gridRound;
+     
+     double transform[];
+     
 	
    CirSim() {
 	theSim = this;
@@ -91,7 +107,6 @@ public class CirSim {
   	width=(int)RootLayoutPanel.get().getOffsetWidth();
   	height=(int)RootLayoutPanel.get().getOffsetHeight();
   	height=height-MENUBARHEIGHT;
-  	width=width-VERTICALPANELWIDTH;
   	
 		if (cv != null) {
 			cv.setWidth(width + "PX");
@@ -99,53 +114,71 @@ public class CirSim {
 			cv.setCoordinateSpaceWidth(width);
 			cv.setCoordinateSpaceHeight(height);
 		}
+		if (backcv != null) {
+			backcv.setWidth(width + "PX");
+			backcv.setHeight(height + "PX");
+			backcv.setCoordinateSpaceWidth(width);
+			backcv.setCoordinateSpaceHeight(height);
+		}
 
-  	setCircuitArea();
+  	//setCircuitArea();
   }
   
   void setCircuitArea() {
   	int height = cv.getCanvasElement().getHeight();
   	int width = cv.getCanvasElement().getWidth();
-		//int h = (int) ((double)height * scopeHeightFraction);
-		/*if (h < 128 && winSize.height > 300)
-		  h = 128;*/
-		//circuitArea = new Rectangle(0, 0, width, height-h);
+	int h = (int) ((double)height * scopeHeightFraction);
+	circuitArea = new Rectangle(0, 0, width, height-h);
   }
  
  public void init() {
 	 
+	 boolean printable = false;
+	 transform = new double[6];
+	 
+	 CircuitElm.initClass(this);
+	 
+	 layoutPanel = new DockLayoutPanel(Unit.PX);
 	 mainBar = new MenuBar(true);
-	// box = new CheckBox();
-	// mainBar.addItem(item);
-	 
-	 MenuBar m = new MenuBar(true);
+	 extrasBar = new MenuBar(true);
 	 
 	 
-		m.addItem(printableCheckItem = new CheckboxMenuItem("White Background",
+	 extrasBar.addItem(printableCheckItem = new CheckboxMenuItem("White Background",
 				new Command() { public void execute(){
 					//int i;
 					//for (i=0;i<scopeCount;i++)
 						//scopes[i].setRect(scopes[i].rect);
 				}}));
-		//printableCheckItem.setState(printable);
-		m.addItem(alternativeColorCheckItem = new CheckboxMenuItem("Alt Color",
+		printableCheckItem.setState(printable);
+		
+		extrasBar.addItem(alternativeColorCheckItem = new CheckboxMenuItem("Alt Color",
 				new Command() { public void execute(){
 
 					//CircuitElm.setColorScale();
 				}}));
 			//alternativeColorCheckItem.setState(getOptionFromStorage("alternativeColor", false));
-			mainBar.addItem("Extras",m);
+		
+			mainBar.addItem("Extras",extrasBar);
+			layoutPanel.addNorth(mainBar, MENUBARHEIGHT);
 	 
-			RootPanel.get().add(mainBar);
-	 
-	 Button b = new Button("Jump!", new ClickHandler() {
-	      public void onClick(ClickEvent event) {
-	        Window.alert("How high?");
-	      }
-	    });
-	 
-	 RootPanel.get().add(b);
-	 
+			
+	  
+			cv = Canvas.createIfSupported();
+			  if (cv==null) {
+				  RootPanel.get().add(new Label("Not working. You need a browser that supports the CANVAS element."));
+				  return;
+			  }
+			  //cvcontext=cv.getContext2d();
+			  //backcv = Canvas.createIfSupported();
+			  //backcontext=backcv.getContext2d();
+	  
+			  
+			  
+			 layoutPanel.add(cv);
+			 setCanvasSize();
+			 RootPanel.get().add(layoutPanel);
+			 
+			 
  }
   
   final Timer timer = new Timer() {
@@ -157,8 +190,35 @@ public class CirSim {
     
     public void updateCircuit() {
     	
+    	
+    	Graphics g=new Graphics(cvcontext);
+    	
+    	if (printableCheckItem.getState()) {
+      	   // CircuitElm.whiteColor = Color.black;
+      	  //  CircuitElm.lightGrayColor = Color.black;
+      	    g.setColor(Color.black);
+    	} else {
+    	   // CircuitElm.whiteColor = Color.white;
+    	   // CircuitElm.lightGrayColor = Color.lightGray;
+    	    g.setColor(Color.black);
+    	}
+    	g.fillRect(0, 0, g.context.getCanvas().getWidth(), g.context.getCanvas().getHeight());
+    	
+    	backcontext.setTransform(transform[0], transform[1], transform[2],
+				 transform[3], transform[4], transform[5]);
+    	
+    	backcontext.setTransform(1, 0, 0, 1, 0, 0);
+    	g.fillRect(0, circuitArea.height, circuitArea.width, cv.getCoordinateSpaceHeight()-circuitArea.height);
+
+
     }
 	
+    void setGrid() {
+	gridSize = 8;
+	gridMask = ~(gridSize-1);
+	gridRound = gridSize/2-1;
+    }
+    
     
     static SafeHtml LSHTML(String s) { return SafeHtmlUtils.fromTrustedString(s); }
 	
